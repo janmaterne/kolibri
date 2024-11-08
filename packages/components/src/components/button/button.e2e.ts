de31f906dfdc6438b2525eb86776e46dfd52769c
Stefan Dietz <mail@stefan-dietz.eu>
2024-11-08 20:07:38 +0100

    Add callback tests for button
    Refs: #6987

--- packages/components/src/components/button/button.e2e.ts
+++ packages/components/src/components/button/button.e2e.ts
@@ -8,6 +8,29 @@ test.describe('kol-button', () => {
 		await expect(kolButton).toContainText('Test Button Element');
 	});
 
+	test.describe('Callbacks', () => {
+		['onClick', 'onMouseDown'].forEach((callbackName) => {
+			test(`should call ${callbackName} when internal button emits`, async ({ page }) => {
+				await page.setContent('<kol-button _label="Button"></kol-button>');
+				const kolButton = page.locator('kol-button');
+
+				const eventPromise = kolButton.evaluate((element: HTMLKolButtonElement, callbackName) => {
+					return new Promise<void>((resolve) => {
+						element._on = {
+							[callbackName]: () => {
+								resolve();
+							},
+						};
+					});
+				}, callbackName);
+				await page.waitForChanges();
+
+				await page.locator('button').click();
+				await expect(eventPromise).resolves.toBeUndefined();
+			});
+		});
+	});
+
 	test.describe('DOM events', () => {
 		['click', 'mousedown'].forEach((event) => {
 			test(`should emit ${event} when internal button emits ${event}`, async ({ page }) => {
