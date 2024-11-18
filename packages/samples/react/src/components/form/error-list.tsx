45c2b27afe314440dc0b5397013c493761ca09bf
Makko <marco.leithold@iso-gruppe.com>
2024-11-18 10:48:24 +0100

    Refactor FormErrorList to use ref for focusing error list and improve structure
    Refs: #6370

--- packages/samples/react/src/components/form/error-list.tsx
+++ packages/samples/react/src/components/form/error-list.tsx
@@ -1,28 +1,37 @@
 import type { FC } from 'react';
-import React from 'react';
+import * as React from 'react';
 import { SampleDescription } from '../SampleDescription';
 import { KolForm, KolInputText } from '@public-ui/react';
 
-export const FormErrorList: FC = () => (
-	<>
-		<SampleDescription>
-			<p>This sample shows a form with error messages.</p>
-		</SampleDescription>
+export const FormErrorList: FC = () => {
+	const formRef = React.useRef<HTMLKolFormElement>(null);
 
-		<KolForm
-			className="w-full"
-			_errorList={[
-				{
-					message: 'Error in Input 2',
-					selector: '#input2',
-				},
-			]}
-		>
-			<div className="grid gap-2">
-				<KolInputText id="input1" _label="Input 1" />
-				<KolInputText id="input2" _label="Input 2" _touched _msg={{ _description: 'Input error', _type: 'error' }} />
-				<KolInputText id="input3" _label="Input 3" />
-			</div>
-		</KolForm>
-	</>
-);
+	React.useEffect(() => {
+		formRef.current?.focusErrorList();
+	}, []);
+
+	return (
+		<>
+			<SampleDescription>
+				<p>This sample shows a form with error messages.</p>
+			</SampleDescription>
+
+			<KolForm
+				ref={formRef}
+				className="w-full"
+				_errorList={[
+					{
+						message: 'Error in Input 2',
+						selector: '#input2',
+					},
+				]}
+			>
+				<div className="grid gap-2">
+					<KolInputText id="input1" _label="Input 1" />
+					<KolInputText id="input2" _label="Input 2" _touched _msg={{ _description: 'Input error', _type: 'error' }} />
+					<KolInputText id="input3" _label="Input 3" />
+				</div>
+			</KolForm>
+		</>
+	);
+};
