c75d3d9355045f7d9054a6c62b2f3e32793874c1
Cyrine Ben Romdhane <135605630+anicyne@users.noreply.github.com>
2024-11-20 11:09:07 +0100

    Update input-password E2E test
    Refs: #7045

--- packages/components/src/components/input-password/input-password.e2e.ts
+++ packages/components/src/components/input-password/input-password.e2e.ts
@@ -2,16 +2,6 @@ import { expect } from '@playwright/test';
 import { test } from '@stencil/playwright';
 
 test.describe('kol-input-password', () => {
-	test.describe('Initial State', () => {
-		test('should render the component correctly', async ({ page }) => {
-			await page.setContent('<kol-input-password _label="Password input" _variant="visibility-toggle" ></kol-input-password>');
-			const input = page.locator('kol-input-password input');
-			const toggleButton = page.locator('.password-toggle-button');
-			await expect(input).toBeVisible();
-			await expect(toggleButton).toBeVisible();
-		});
-	});
-
 	test.describe('Password Visibility Toggle', () => {
 		test('should toggle the password visibility when button is clicked', async ({ page }) => {
 			await page.setContent('<kol-input-password _label="Password input" _variant="visibility-toggle"></kol-input-password>');
