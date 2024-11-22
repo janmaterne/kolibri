683050f4fe1e880b6ec86da302bca50dc2389c81
Makko <marco.leithold@iso-gruppe.com>
2024-11-22 15:00:24 +0100

    fix: update textarea resize options and migration tasks
    Refs: #6527

--- packages/tools/kolibri-cli/src/migrate/runner/tasks/v3/textarea.ts
+++ packages/tools/kolibri-cli/src/migrate/runner/tasks/v3/textarea.ts
@@ -1,4 +1,4 @@
 import { UpdatePropertyValueTask } from '../common/UpdatePropertyValueTask';
 
-export const TextareaUpdatePropertyValue_Resize_Both = UpdatePropertyValueTask.getInstance('kol-textarea', '_resize', 'both', 'vertical', '^2');
-export const TextareaUpdatePropertyValue_Resize_Horizontal = UpdatePropertyValueTask.getInstance('kol-textarea', '_resize', 'horizontal', 'none', '^2');
+export const TextareaUpdatePropertyValue_Resize_Both = UpdatePropertyValueTask.getInstance('kol-textarea', '_resize', 'both', 'vertical', '>=2 <3');
+export const TextareaUpdatePropertyValue_Resize_Horizontal = UpdatePropertyValueTask.getInstance('kol-textarea', '_resize', 'horizontal', 'none', '>=2 <3');
