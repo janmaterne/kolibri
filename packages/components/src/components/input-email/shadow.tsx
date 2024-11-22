6b2dc798859ac226f95506d4582b379bee3d3fe0
Makko <marco.leithold@iso-gruppe.com>
2024-11-22 09:51:35 +0100

    refactor: remove unused render method and related logic from KolInputEmail component
    Refs: #7032

--- packages/components/src/components/input-email/shadow.tsx
+++ packages/components/src/components/input-email/shadow.tsx
@@ -1,5 +1,5 @@
 import type { JSX } from '@stencil/core';
-import { Component, Element, Fragment, h, Host, Method, Prop, State, Watch } from '@stencil/core';
+import { Component, Element, h, Host, Method, Prop, State, Watch } from '@stencil/core';
 import clsx from 'clsx';
 
 import type {
@@ -22,17 +22,14 @@ import type {
 	SyncValueBySelectorPropType,
 	TooltipAlignPropType,
 } from '../../schema';
-import { buildBadgeTextString, setState, showExpertSlot } from '../../schema';
+import { setState } from '../../schema';
 
 import { nonce } from '../../utils/dev.utils';
 import { propagateSubmitEventToForm } from '../form/controller';
-import { getRenderStates } from '../input/controller';
-import { InternalUnderlinedBadgeText } from '../../functional-components';
 import KolFormFieldFc, { type FormFieldStateWrapperProps } from '../../functional-component-wrappers/FormFieldStateWrapper';
 import KolInputFc, { type InputStateWrapperProps } from '../../functional-component-wrappers/InputStateWrapper';
 import KolInputContainerFc from '../../functional-component-wrappers/InputContainerStateWrapper';
 import { InputEmailController } from './controller';
-import { KolInputTag } from '../../core/component-names';
 
 /**
  * @slot - Die Beschriftung des Eingabefeldes.
@@ -128,98 +125,6 @@ export class KolInputEmail implements InputEmailAPI, FocusableElement {
 						<KolInputFc {...this.getInputProps()} />
 					</KolInputContainerFc>
 				</KolFormFieldFc>
-			</Host>
-		);
-	}
-
-	public render2(): JSX.Element {
-		const { ariaDescribedBy } = getRenderStates(this.state);
-		const hasSuggestions = Array.isArray(this.state._suggestions) && this.state._suggestions.length > 0;
-		const hasExpertSlot = showExpertSlot(this.state._label);
-
-		return (
-			<Host
-				class={{
-					'kol-input-email': true,
-					'has-value': this.state._hasValue,
-				}}
-			>
-				<KolInputTag
-					class={{ email: true, 'hide-label': !!this.state._hideLabel }}
-					_accessKey={this.state._accessKey}
-					_alert={this.showAsAlert()}
-					_currentLength={this.state._currentLength}
-					_disabled={this.state._disabled}
-					_msg={this.state._msg}
-					_hideError={this.state._hideError}
-					_hasCounter={this.state._hasCounter}
-					_hideLabel={this.state._hideLabel}
-					_hint={this.state._hint}
-					_icons={this.state._icons}
-					_id={this.state._id}
-					_label={this.state._label}
-					_suggestions={this.state._suggestions}
-					_maxLength={this.state._maxLength}
-					_readOnly={this.state._readOnly}
-					_required={this.state._required}
-					_shortKey={this.state._shortKey}
-					_smartButton={this.state._smartButton}
-					_tooltipAlign={this._tooltipAlign}
-					_touched={this.state._touched}
-					onClick={() => this.inputRef?.focus()}
-					role={`presentation` /* Avoid element being read as 'clickable' in NVDA */}
-				>
-					<span slot="label">
-						{hasExpertSlot ? (
-							<slot name="expert"></slot>
-						) : typeof this.state._accessKey === 'string' || typeof this.state._shortKey === 'string' ? (
-							<>
-								<InternalUnderlinedBadgeText badgeText={buildBadgeTextString(this.state._accessKey, this.state._shortKey)} label={this.state._label} />{' '}
-								<span class="access-key-hint" aria-hidden="true">
-									{buildBadgeTextString(this.state._accessKey, this.state._shortKey)}
-								</span>
-							</>
-						) : (
-							<span>{this.state._label}</span>
-						)}
-					</span>
-					<div slot="input">
-						<input
-							ref={this.catchRef}
-							title=""
-							accessKey={this.state._accessKey}
-							aria-describedby={ariaDescribedBy.length > 0 ? ariaDescribedBy.join(' ') : undefined}
-							aria-label={this.state._hideLabel && typeof this.state._label === 'string' ? this.state._label : undefined}
-							autoCapitalize="off"
-							autoComplete={this.state._autoComplete}
-							autoCorrect="off"
-							disabled={this.state._disabled}
-							multiple={this.state._multiple}
-							id={this.state._id}
-							list={hasSuggestions ? `${this.state._id}-list` : undefined}
-							maxlength={this.state._maxLength}
-							name={this.state._name}
-							pattern={this.state._pattern}
-							placeholder={this.state._placeholder}
-							readOnly={this.state._readOnly}
-							required={this.state._required}
-							spellcheck="false"
-							type="email"
-							value={this.state._value as string}
-							{...this.controller.onFacade}
-							onKeyDown={this.onKeyDown}
-							onInput={this.onInput}
-							onFocus={(event) => {
-								this.controller.onFacade.onFocus(event);
-								this.inputHasFocus = true;
-							}}
-							onBlur={(event) => {
-								this.controller.onFacade.onBlur(event);
-								this.inputHasFocus = false;
-							}}
-						/>
-					</div>
-				</KolInputTag>
 			</Host>
 		);
 	}
@@ -396,13 +301,6 @@ export class KolInputEmail implements InputEmailAPI, FocusableElement {
 
 	public constructor() {
 		this.controller = new InputEmailController(this, 'email', this.host);
-	}
-
-	private showAsAlert(): boolean {
-		if (this.state._alert === undefined) {
-			return Boolean(this.state._touched) && !this.inputHasFocus;
-		}
-		return this.state._alert;
 	}
 
 	@Watch('_accessKey')
