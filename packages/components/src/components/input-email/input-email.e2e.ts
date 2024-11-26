c6fd030479841d9d108284a4044c193a8461355e
Stefan Dietz <mail@stefan-dietz.eu>
2024-11-26 18:57:57 +0100

    refactor: Use more constants
    Refs: #6987

--- packages/components/src/components/input-email/input-email.e2e.ts
+++ packages/components/src/components/input-email/input-email.e2e.ts
@@ -1,8 +1,11 @@
 import { test } from '@stencil/playwright';
 import { testInputCallbacks, testInputDomEvents, testInputValueReflection } from '../../e2e';
 
-test.describe('kol-input-email', () => {
-	testInputValueReflection<HTMLKolInputEmailElement>('kol-input-email', 'example@example.com');
-	testInputCallbacks<HTMLKolInputEmailElement>('kol-input-email');
-	testInputDomEvents('kol-input-email');
+const COMPONENT_NAME = 'kol-input-email';
+const TEST_VALUE = 'example@example.com';
+
+test.describe(COMPONENT_NAME, () => {
+	testInputValueReflection<HTMLKolInputEmailElement>(COMPONENT_NAME, TEST_VALUE);
+	testInputCallbacks<HTMLKolInputEmailElement>(COMPONENT_NAME);
+	testInputDomEvents(COMPONENT_NAME);
 });
--- packages/components/src/components/input-text/input-text.e2e.ts
+++ packages/components/src/components/input-text/input-text.e2e.ts
@@ -2,6 +2,9 @@ import { expect } from '@playwright/test';
 import { test } from '@stencil/playwright';
 import { testInputCallbacks, testInputDomEvents, testInputValueReflection } from '../../e2e';
 
+const COMPONENT_NAME = 'kol-input-text';
+const TEST_VALUE = 'Hello World';
+
 test.describe('kol-input-text', () => {
 	test.describe('smart-button', () => {
 		test('should have smart-button', async ({ page }) => {
@@ -18,7 +21,7 @@ test.describe('kol-input-text', () => {
 		});
 	});
 
-	testInputValueReflection<HTMLKolInputTextElement>('kol-input-text', 'Hello World');
-	testInputCallbacks<HTMLKolInputTextElement>('kol-input-text');
-	testInputDomEvents('kol-input-text');
+	testInputValueReflection<HTMLKolInputTextElement>(COMPONENT_NAME, TEST_VALUE);
+	testInputCallbacks<HTMLKolInputTextElement>(COMPONENT_NAME);
+	testInputDomEvents(COMPONENT_NAME);
 });
