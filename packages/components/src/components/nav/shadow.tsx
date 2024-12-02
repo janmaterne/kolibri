30f625b900d139ba5d619d437421a7435d803d7f
Cyrine Ben Romdhane <135605630+anicyne@users.noreply.github.com>
2024-12-02 14:33:55 +0100

    Refactor nav
    Refs: #7036

--- packages/components/src/components/nav/shadow.tsx
+++ packages/components/src/components/nav/shadow.tsx
@@ -129,7 +129,7 @@ export class KolNav implements NavAPI {
 	private expandButton(collapsible: boolean, link: ButtonWithChildrenProps, expanded: boolean): JSX.Element {
 		return (
 			<KolButtonWcTag
-				class="kol-nav__expand-button"
+				class="kol-nav__entry--expand-button"
 				_ariaExpanded={expanded}
 				_disabled={!collapsible}
 				_icons={'codicon codicon-' + (expanded ? 'remove' : 'add')}
