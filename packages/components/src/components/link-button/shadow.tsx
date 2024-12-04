6685be2530c8fb1117843c8b25489988f58324b3
alex_ <alexander.schmidt@codeagle.de>
2024-12-04 09:08:04 +0100

    review changes
    Refs: #7036

--- packages/components/src/components/link-button/shadow.tsx
+++ packages/components/src/components/link-button/shadow.tsx
@@ -17,9 +17,8 @@ import type {
 	TooltipAlignPropType,
 } from '../../schema';
 import type { JSX } from '@stencil/core';
-import { Component, h, Host, Method, Prop } from '@stencil/core';
+import { Component, h, Method, Prop } from '@stencil/core';
 import { KolLinkWcTag } from '../../core/component-names';
-import clsx from 'clsx';
 
 @Component({
 	tag: 'kol-link-button',
@@ -52,32 +51,30 @@ export class KolLinkButton implements LinkButtonProps, FocusableElement {
 
 	public render(): JSX.Element {
 		return (
-			<Host class="kol-link-button">
-				<KolLinkWcTag
-					ref={this.catchRef}
-					class={clsx('kol-link-button', {
-						[`kol-link--${this._variant as string}`]: this._variant !== 'custom',
-						[this._customClass as string]: this._variant === 'custom' && typeof this._customClass === 'string' && this._customClass.length > 0,
-					})}
-					_accessKey={this._accessKey}
-					_ariaCurrentValue={this._ariaCurrentValue}
-					_ariaDescription={this._ariaDescription}
-					_disabled={this._disabled}
-					_download={this._download}
-					_hideLabel={this._hideLabel}
-					_href={this._href}
-					_icons={this._icons}
-					_label={this._label}
-					_on={this._on}
-					_role="button"
-					_shortKey={this._shortKey}
-					_tabIndex={this._tabIndex}
-					_target={this._target}
-					_tooltipAlign={this._tooltipAlign}
-				>
-					<slot name="expert" slot="expert"></slot>
-				</KolLinkWcTag>
-			</Host>
+			<KolLinkWcTag
+				ref={this.catchRef}
+				class={{
+					[`kol-link--${this._variant as string}`]: this._variant !== 'custom',
+					[this._customClass as string]: this._variant === 'custom' && typeof this._customClass === 'string' && this._customClass.length > 0,
+				}}
+				_accessKey={this._accessKey}
+				_ariaCurrentValue={this._ariaCurrentValue}
+				_ariaDescription={this._ariaDescription}
+				_disabled={this._disabled}
+				_download={this._download}
+				_hideLabel={this._hideLabel}
+				_href={this._href}
+				_icons={this._icons}
+				_label={this._label}
+				_on={this._on}
+				_role="button"
+				_shortKey={this._shortKey}
+				_tabIndex={this._tabIndex}
+				_target={this._target}
+				_tooltipAlign={this._tooltipAlign}
+			>
+				<slot name="expert" slot="expert"></slot>
+			</KolLinkWcTag>
 		);
 	}
 
